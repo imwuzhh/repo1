55be7923aa6f72622d61b1a95b5fb40a2f0a5e36
harrywu <wuzh1230@163.com>
2014-04-17 07:21:51 +0800

    add test messager.

---
+++ contrib/messager/messager/messager.cpp
@@ -0,0 +1,114 @@
+// messager.cpp : 定义控制台应用程序的入口点。
+//
+
+#pragma warning(disable: 4996)
+#include <Windows.h>
+#include <ShlObj.h>
+#include <string>
+
+#define ID_FILE_PREV                    16
+#define ID_FILE_NEXT                    17
+#define ID_FILE_SEARCH                  18
+
+#define WM_USER_PREV_PAGE    (WM_USER + 0x1001)
+#define WM_USER_NEXT_PAGE    (WM_USER + 0x1002)
+#define WM_USER_SEARCH       (WM_USER + 0x1003)
+
+struct EnumParam {
+    char * className;
+    char * title;
+    DWORD userdata;
+    DWORD returnValue;
+};
+
+
+BOOL CALLBACK ChildWindowEnumProc(HWND hWnd, LPARAM lParam)
+{
+    EnumParam * ep = (EnumParam *)lParam;
+
+    char szClassName [100] = "";
+    char szTitle [100] = "";
+    GetClassNameA(hWnd, szClassName, 100);
+    GetWindowTextA(hWnd, szTitle, 100);
+    DWORD userdata = GetWindowLongPtrA(hWnd, GWLP_USERDATA);
+
+    if (0){
+        std::string debugstr = szClassName;
+        debugstr += szTitle;
+        debugstr += "\n";
+        OutputDebugStringA(debugstr.c_str());
+    }
+
+    if (!stricmp(szTitle, ep->title) && !stricmp(szClassName, ep->className)
+        && (ep->userdata && ep->userdata == userdata || ep->userdata == 0)){
+            ep->returnValue = (DWORD)hWnd;
+            return FALSE; // Found, quit.
+    }
+    return TRUE;
+}
+
+BOOL CALLBACK TopWindowEnumProc(HWND hWnd, LPARAM lParam)
+{
+    EnumParam * ep = (EnumParam *)lParam;
+
+    char szClassName [100] = "";
+    char szTitle [100] = "";
+    GetClassNameA(hWnd, szClassName, 100);
+    GetWindowTextA(hWnd, szTitle, 100);
+    DWORD userdata = GetWindowLongPtrA(hWnd, GWLP_USERDATA);
+
+    if (0){
+        std::string debugstr = szClassName;
+        debugstr += szTitle;
+        debugstr += "\n";
+        OutputDebugStringA(debugstr.c_str());
+    }
+
+    if (!stricmp(szTitle, ep->title) && !stricmp(szClassName, ep->className)
+        && (ep->userdata && ep->userdata == userdata || ep->userdata == 0)){
+            ep->returnValue = (DWORD)hWnd;
+            return FALSE; // Found, quit.
+    }
+
+    // Go on to search its children.
+    EnumChildWindows(hWnd, ChildWindowEnumProc, lParam);
+    if (ep->returnValue){
+        return FALSE;
+    }
+
+    return TRUE; // to continue enumerate
+}
+
+HWND FindChildWindow(const char * classname, const char * title, DWORD userdata)
+{
+    EnumParam ep = {0};
+    ep.className = strdup(classname);
+    ep.title = strdup(title);
+    ep.userdata = userdata;
+    ep.returnValue = 0ul;
+    EnumWindows(TopWindowEnumProc, (LPARAM)&ep);
+    free(ep.className); free(ep.title);
+    return (HWND) ep.returnValue;
+}
+
+
+int main(int argc, char * argv[])
+{
+    while (TRUE)
+    {
+        HWND hMsgWnd = FindChildWindow("SHELLDLL_DefView", "ShellView", 0xED0CED0C);
+        if (hMsgWnd == NULL) return 0;
+        printf("0->Prev, 1->Next, 2->Search\n>");
+        char c = getchar(); UINT cmd = WM_USER_NEXT_PAGE;
+        switch (c)
+        {
+            case '0': cmd = WM_USER_NEXT_PAGE; break;
+            case '1': cmd = WM_USER_PREV_PAGE; break;
+            case '2': cmd = WM_USER_SEARCH; break;
+            default: break;
+        }
+        SendMessage(hMsgWnd, cmd, (WPARAM)0x0001a220, (LPARAM)0);
+    }
+	return 0;
+}
+

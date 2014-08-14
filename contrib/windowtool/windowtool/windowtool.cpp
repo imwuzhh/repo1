1040a86ed7be791fa4aa8cfd6502d305c61ecc4d
harrywu <imwuzhh@gmail.com>
2014-08-14 16:49:54 +0800

    add contrib/windowtool utility to remove search bar of system.

---
+++ contrib/windowtool/windowtool/windowtool.cpp
@@ -0,0 +1,12 @@
+// windowtool.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    ShowWindow((HWND)0x00121686, SW_HIDE);
+	return 0;
+}
+
